96bf3144a5e170dfc9c08f6dfbc70e2bc3171ad8
m sanjay narasimha <150546961+sanjaynarasimha@users.noreply.github.com>
2024-01-16 19:41:47 +0530

    Initial commit

---
+++ _Android_group_1/java/exportkit/figma/group_1_activity.java
@@ -0,0 +1,42 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		group_1
+	 *	@date 		Tuesday 16th of January 2024 02:00:32 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class group_1_activity extends Activity {
+
+	
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.group_1);
+
+		
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
---
+++ _Android_group_1/java/exportkit/figma/screen1_activity.java
@@ -0,0 +1,80 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		group_1
+	 *	@date 		Tuesday 16th of January 2024 02:00:32 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class screen1_activity extends Activity {
+
+	
+	private View _bg__screen1_ek2;
+	private ImageView vector;
+	private View _bg__battery_full_ek1;
+	private ImageView vector_ek1;
+	private View _bg__wifi_ek1;
+	private TextView welcome_onboard;
+	private View rectangle_1;
+	private TextView register;
+	private View rectangle_2;
+	private View rectangle_3;
+	private View rectangle_4;
+	private View rectangle_5;
+	private TextView enter_your_name;
+	private TextView enter_you_number;
+	private TextView enter_your_email;
+	private TextView enter_your_password;
+	private TextView routine_app;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.screen1);
+
+		
+		_bg__screen1_ek2 = (View) findViewById(R.id._bg__screen1_ek2);
+		vector = (ImageView) findViewById(R.id.vector);
+		_bg__battery_full_ek1 = (View) findViewById(R.id._bg__battery_full_ek1);
+		vector_ek1 = (ImageView) findViewById(R.id.vector_ek1);
+		_bg__wifi_ek1 = (View) findViewById(R.id._bg__wifi_ek1);
+		welcome_onboard = (TextView) findViewById(R.id.welcome_onboard);
+		rectangle_1 = (View) findViewById(R.id.rectangle_1);
+		register = (TextView) findViewById(R.id.register);
+		rectangle_2 = (View) findViewById(R.id.rectangle_2);
+		rectangle_3 = (View) findViewById(R.id.rectangle_3);
+		rectangle_4 = (View) findViewById(R.id.rectangle_4);
+		rectangle_5 = (View) findViewById(R.id.rectangle_5);
+		enter_your_name = (TextView) findViewById(R.id.enter_your_name);
+		enter_you_number = (TextView) findViewById(R.id.enter_you_number);
+		enter_your_email = (TextView) findViewById(R.id.enter_your_email);
+		enter_your_password = (TextView) findViewById(R.id.enter_your_password);
+		routine_app = (TextView) findViewById(R.id.routine_app);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
